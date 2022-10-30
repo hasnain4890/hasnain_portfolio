cd4ee64dfa0a74530fd103cb2e10c83bee8e0ed4
hasnain ahmad <hasnainahmad4890@gmail.com>
2022-10-30 23:51:25 +0500

    MY FIRST PORTFOLIO

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("scrip running");
+document.querySelector('.cross').style.display='none';
+document.querySelector('.hamburger').addEventListener("click",()=>{
+    document.querySelector('.sidebar').classList.toggle('sidebarGo');
+    if( document.querySelector('.sidebar').classList.contains('sidebarGo')){
+    document.querySelector('.ham').style.display='inline'
+    document.querySelector('.cross').style.display='none'}   
+       else{
+        document.querySelector('.cross').style.display='inline'
+    document.querySelector('.ham').style.display='none'  
+       } 
+    
+})
